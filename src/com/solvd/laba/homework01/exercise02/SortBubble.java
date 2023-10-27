66cd9b6da631eb4ce969f644dcf48b6d7e120131
Mateusz Kacprzak <mkacprzak.laba@solvd.com>
2023-10-27 15:32:44 +0200

    finished exercise 2 from homework 1

---
+++ src/com/solvd/laba/homework01/exercise02/SortBubble.java
@@ -0,0 +1,15 @@
+package com.solvd.laba.homework01.exercise02;
+
+public class SortBubble {
+    public static void sort(int[] array) {
+        for (int i = (array.length - 1); i > 0; --i) {
+            for (int j = 0; j < i; ++j) {
+                if (array[j] > array[j+1]) {
+                    int swapHelper = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = swapHelper;
+                }
+            }
+        }
+    }
+}
