d72f95442f750c8167da3d723e35437ac3f2d939
Mateusz Kacprzak <mkacprzak.laba@solvd.com>
2023-12-06 16:15:00 +0100

    started working on Reflections

---
+++ src/main/java/com/solvd/laba/homework10/exercise02/Reflections.java
@@ -0,0 +1,80 @@
+package com.solvd.laba.homework10.exercise02;
+
+import java.lang.reflect.*;
+
+public class Reflections {
+    public static void main(String[] args) {
+        // print class details
+        try {
+            printClassDetails("com.solvd.laba.homework02.exercise01.Address");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found. Details: " + e.toString());
+        }
+
+        // create object and call method from reflection
+        try {
+            Class<?> cl = Class.forName("com.solvd.laba.homework02.exercise01.Person");
+            Object newInstance = cl.newInstance();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found. Details: " + e.toString());
+        } catch (InstantiationException e) {
+            System.out.println("Unable to instantiate the class. Details: " + e.toString());
+        } catch (IllegalAccessException e) {
+            System.out.println("Member or constructor not accessible. Details: " + e.toString());
+        }
+    }
+
+    public static void printClassDetails(String className) throws ClassNotFoundException {
+        Class<?> cl = Class.forName(className);
+        System.out.println("Class name:      " + cl.getName());
+        System.out.println("Class modifiers: " + Modifier.toString(cl.getModifiers()));
+        System.out.println("Class fields:");
+        for (Field field : cl.getDeclaredFields()) {
+            System.out.println("- " + fieldToString(field));
+        }
+        System.out.println("Class constructors:");
+        for (Constructor<?> constructor : cl.getDeclaredConstructors()) {
+            System.out.println("- " + constructorToString(constructor));
+        }
+        System.out.println("Class methods:");
+        for (Method method : cl.getDeclaredMethods()) {
+            System.out.println("- " + methodToString(method));
+        }
+    }
+
+    public static String fieldToString(Field field) {
+        return Modifier.toString(field.getModifiers())
+                + " " + field.getType().getName()
+                + " " + field.getName();
+    }
+
+    public static String constructorToString(Constructor<?> constructor) {
+        String result = Modifier.toString(constructor.getModifiers()) + " (";
+
+        boolean firstParameter = true;
+        for (Class<?> cl : constructor.getParameterTypes()) {
+            result += firstParameter ? "" : ", ";
+            result += cl.getCanonicalName();
+            firstParameter = false;
+        }
+        result += ")";
+        return result;
+    }
+
+    public static String methodToString(Method method) {
+        String result = Modifier.toString(method.getModifiers())
+                + " " + method.getName()
+                + "(";
+
+        boolean firstParameter = true;
+        for (Parameter parameter : method.getParameters()) {
+            result += firstParameter ? "" : ", ";
+            result += parameter.getType().getCanonicalName();
+            result += " " + parameter.getName();
+            firstParameter = false;
+        }
+        result += ")";
+        return result;
+    }
+
+}
